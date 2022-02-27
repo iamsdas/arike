a85e55500066e2e9966db0bbeb134a43891d91f7
Suryashankar Das <suryashankardas.2002@gmail.com>
2022-02-27 10:28:22 +0530

    add facilities list and detail view

--- arike/facilities/urls.py
+++ arike/facilities/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
-from arike.facilities.views import FacilityCreateView
+from arike.facilities.views import (
+    FacilityCreateView,
+    GenericFacilityListVeiw,
+    GenericFacilityDetailView,
+)
 
 app_name = "facilities"
 urlpatterns = [
+    path("list/", view=GenericFacilityListVeiw.as_view(), name="list"),
     path("create/", view=FacilityCreateView.as_view(), name="create"),
+    path("<pk>/", view=GenericFacilityDetailView.as_view()),
 ]
--- arike/facilities/views.py
+++ arike/facilities/views.py
@@ -1,15 +1,16 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 from arike.facilities.forms import FacilityCreationForm
 from arike.facilities.models import Facility
-from django.views.generic import ListView, View
 
 
 class GenericFacilityFormView(LoginRequiredMixin):
     form_class = FacilityCreationForm
     template_name = "facilities/form.html"
-    success_url = "/"
+    success_url = "/facilities/list/"
 
     def get_queryset(self):
         return Facility.objects.all()
@@ -30,6 +31,12 @@ class FacilityDeleteView(GenericFacilityFormView, DeleteView):
 class GenericFacilityListVeiw(ListView, LoginRequiredMixin):
     model = Facility
     template_name = "facilities/list.html"
+    context_object_name = "facilities"
 
     def get_queryset(self):
         return Facility.objects.all()
+
+
+class GenericFacilityDetailView(DetailView):
+    model = Facility
+    template_name = "facilities/detail.html"
