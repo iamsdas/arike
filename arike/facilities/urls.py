640bf23fddb5c27b80fe4537184457c49ff5fe53
Suryashankar Das <suryashankardas.2002@gmail.com>
2022-03-01 10:41:45 +0530

    update facility detail page UI

--- arike/facilities/urls.py
+++ arike/facilities/urls.py
@@ -2,13 +2,15 @@ from django.urls import path
 
 from arike.facilities.views import (
     FacilityCreateView,
-    GenericFacilityListVeiw,
+    FacilityUpdateView,
     GenericFacilityDetailView,
+    GenericFacilityListVeiw,
 )
 
 app_name = "facilities"
 urlpatterns = [
     path("list/", view=GenericFacilityListVeiw.as_view(), name="list"),
     path("create/", view=FacilityCreateView.as_view(), name="create"),
+    path("update/<pk>/", view=FacilityUpdateView.as_view(), name="update"),
     path("<pk>/", view=GenericFacilityDetailView.as_view()),
 ]
